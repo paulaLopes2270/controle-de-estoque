7f8c8b34456b145386aec36e0f7eb8d7f08db3dc
Paula <plnhschltz@gmail.com>
2023-02-23 17:51:45 -0300

    finsh header

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import {
 	MenuButtonContainer,
-	NavMenuButton,
+	SidebarBackground,
 	SidebarElement,
 	SlideSideBar,
 } from "./style";
-import { MdAddBusiness } from "react-icons/md";
+import { MdAddBusiness, MdHome } from "react-icons/md";
+import { IoIosHome } from "react-icons/io";
 import { MenuButton } from "./components";
 
 export const Sidebar = () => {
@@ -14,15 +16,25 @@ export const Sidebar = () => {
 	return (
 		<SidebarElement>
 			<SlideSideBar isOpen={isOpen}>
+				<MenuButtonContainer isOpen={isOpen}>
+					<MenuButton useButton={[isOpen, setIsOpen]} />
+				</MenuButtonContainer>
 				<nav>
-					<MenuButtonContainer>
-						<MenuButton useButton={[isOpen, setIsOpen]} />
-					</MenuButtonContainer>
-					<NavMenuButton>
+					<Link
+						to="/"
+						onClick={() => setIsOpen(false)}>
+						<IoIosHome size={iconSize} />
+						<span>Home</span>
+					</Link>
+					<Link
+						to="/"
+						onClick={() => setIsOpen(false)}>
 						<MdAddBusiness size={iconSize} />
-					</NavMenuButton>
+						<span>Cadastro de produtos</span>
+					</Link>
 				</nav>
 			</SlideSideBar>
+			<SidebarBackground isOpen={isOpen} onClick={() => setIsOpen(false)} />
 		</SidebarElement>
 	);
 };
--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const SidebarElement = styled.aside`
+	position: relative;
 	height: 100vh;
 	width: 50px;
 	z-index: 1000;
@@ -11,45 +12,87 @@ interface ISlideSideBar {
 }
 
 export const SlideSideBar = styled.div<ISlideSideBar>`
+	position: absolute;
+	left: 0;
+	top: 0;
 	width: ${({ isOpen }) => (isOpen ? "100vw" : "100%")};
 	height: 100%;
 	background: white;
 	box-shadow: 0 3px 3px gray;
 	transition: 0.3s;
+	z-index: 1;
 
 	@media screen and (min-width: 500px) {
-		width: ${({ isOpen }) => (isOpen ? "300px" : "100%")};
+		width: ${({ isOpen }) => (isOpen ? "250px" : "100%")};
 	}
 	nav {
 		display: flex;
 		justify-content: center;
 		flex-wrap: wrap;
 		gap: 10px;
-    overflow: hidden;
+		overflow: hidden;
+		a {
+			flex: 1 1 100%;
+			display: flex;
+			justify-content: ${({ isOpen }) => (isOpen ? "flex-start" : "center")};
+			gap: ${({ isOpen }) => (isOpen ? "15px" : "0")};
+			align-items: center;
+			margin: 0;
+			padding: ${({ isOpen }) => (isOpen ? "5px 5px 5px 10px" : "5px")};
+			border: none;
+			background: transparent;
+			outline: none;
+			color: black;
+			overflow: hidden;
+			text-decoration: none;
+			font-size: 18px;
+			transform-origin: center;
+			/* box-shadow: 0px 2px 1px 0 gray; */
+			cursor: pointer;
+
+			:hover {
+				transform: scale(1.03);
+			}
+
+			span {
+				width: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+				overflow: hidden;
+				transition: 0.3s;
+				white-space: nowrap;
+			}
+		}
 	}
 `;
 
-export const MenuButtonContainer = styled.div`
+interface IMenuButtonContainer {
+	isOpen: boolean;
+}
+
+export const MenuButtonContainer = styled.div<IMenuButtonContainer>`
 	flex: 1 1 100%;
 	display: flex;
-	justify-content: flex-end;
+	justify-content: ${({ isOpen }) => (isOpen ? "flex-end" : "center")};
 	transform-origin: 100%;
 	padding: 5px;
+	margin-bottom: 50px;
 	:hover {
 		transform: scale(1.03);
 	}
 `;
 
-export const NavMenuButton = styled.button`
-	flex: 1 1 100%;
-	display: flex;
-	justify-content: flex-end;
-	gap: 10px;
-	margin: 0;
-	padding: 5px;
-	border: none;
-	background: transparent;
-	outline: none;
-	/* box-shadow: 0px 2px 1px 0 gray; */
-	cursor: pointer;
+interface ISidebarBackground {
+	isOpen: boolean;
+}
+export const SidebarBackground = styled.div<ISidebarBackground>`
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100vw;
+	height: 100%;
+	min-height: 100vh;
+	background: black;
+	opacity: ${({ isOpen }) => (isOpen ? "0.3" : "0")};
+	visibility: ${({ isOpen }) => (isOpen ? "visible" : "hidden")};
+	transition: 0.3s;
+	z-index: 0;
 `;
